2e3eae6fc07319cc162901c0a4440aac653e61d7
Vaannilavan Pathmathas <194982081+vaannilavan@users.noreply.github.com>
2025-02-23 14:25:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.text', {
+    strings: ['Front-end Developer', 'Back-end Developer','Web Designer', 'UI/UX Designer' ],
+    typeSpeed: 50,
+    backSpeed: 10,
+    loop: true
+});
+
+
+function toggleMode() {
+    (document.getElementById('dark-mode')).disabled = !(document.getElementById('dark-mode')).disabled;
+}
+
